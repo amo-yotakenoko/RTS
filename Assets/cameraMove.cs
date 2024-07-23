05ee8153ad4fe298bac76dbc99f514805f641c72
amo-yotakenoko <53008173+amo-yotakenoko@users.noreply.github.com>
2024-07-23 10:45:39 +0900

    カメラ移動

---
+++ Assets/cameraMove.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraMove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start() { }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position += new Vector3(
+            Input.GetAxis("Horizontal"),
+            0,
+            Input.GetAxis("Vertical")
+        );
+    }
+}
