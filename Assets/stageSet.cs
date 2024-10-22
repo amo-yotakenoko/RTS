f08a38fb897dfa79b75694398496a66d10043007
amo-yotakenoko <53008173+amo-yotakenoko@users.noreply.github.com>
2024-10-22 14:20:24 +0900

    ちょっと軽く

--- Assets/stageSet.cs
+++ Assets/stageSet.cs
@@ -6,26 +6,27 @@ public class stageSet : MonoBehaviour
 {
     // Start is called before the first frame update
     public GameObject character;
+    public GameObject builder;
 
     void Start()
     {
-        StartCoroutine(setCharacter(1, new Vector3(-50, 0, -50)));
-        StartCoroutine(setCharacter(2, new Vector3(50, 0, -50)));
-        StartCoroutine(setCharacter(3, new Vector3(-50, 0, 50)));
-        StartCoroutine(setCharacter(4, new Vector3(50, 0, 50)));
+        StartCoroutine(setCharacter(1, new Vector3(-75, 0, -75)));
+        StartCoroutine(setCharacter(2, new Vector3(75, 0, -75)));
+        StartCoroutine(setCharacter(3, new Vector3(-75, 0, 75)));
+        StartCoroutine(setCharacter(4, new Vector3(75, 0, 75)));
     }
 
     IEnumerator setCharacter(int team, Vector3 position)
     {
         for (int i = 0; i < 100; i++)
         {
-            var instance = Instantiate(character);
+            var instance = Instantiate(i < 90 ? character : builder);
 
             instance.transform.position = position;
             instance.transform.position += new Vector3(
-                Random.Range(-5.0f, 5.0f),
+                Random.Range(-1.0f, 1.0f),
                 0,
-                Random.Range(-5.0f, 5.0f)
+                Random.Range(-1.0f, 1.0f)
             );
 
             instance.GetComponent<Entity>().team = team;
